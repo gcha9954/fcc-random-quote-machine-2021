d3f3d7e75692c19e0ce7126da20c047ce054fb97
Robert Dupplaw <36577903+gcha9954@users.noreply.github.com>
2021-02-10 19:13:25 +0000

    formatting and reorganised functions

--- script.js
+++ script.js
@@ -6,22 +6,31 @@ const getQuotes = () => {
     .then((data) => {
       let max = data.length;
       let min = 0;
-      let randomInt = Math.floor(Math.random() * (max - min) + min)
-      document.getElementById("text").innerHTML = data[randomInt].text;
-      document.getElementById("author").innerHTML = data[randomInt].author || "Anonymous";
-      document.getElementById('tweet-quote').setAttribute('href', `https://twitter.com/intent/tweet?text=${data[randomInt].text} - ${data[randomInt].author || "Anonymous"}`);
+      let randomInt = Math.floor(Math.random() * (max - min) + min);
+      updateHTML(data[randomInt].text, data[randomInt].author);
       quotesArray = data;
-    })
-}
+    });
+};
 
 const newQuoteFromArray = () => {
   let max = quotesArray.length;
   let min = 0;
-  let randomInt = Math.floor(Math.random() * (max - min) + min)
-  document.getElementById("text").innerHTML = quotesArray[randomInt].text;
-  document.getElementById("author").innerHTML = quotesArray[randomInt].author || "Anonymous";
-  document.getElementById('tweet-quote').setAttribute('href', `https://twitter.com/intent/tweet?text=${quotesArray[randomInt].text} - ${quotesArray[randomInt].author || "Anonymous"}`)
-}
+  let randomInt = Math.floor(Math.random() * (max - min) + min);
+  updateHTML(quotesArray[randomInt].text, quotesArray[randomInt].author);
+};
+
+const updateHTML = (text, author) => {
+  document.getElementById("text").innerHTML = text;
+  document.getElementById("author").innerHTML = author || "Anonymous";
+  document
+    .getElementById("tweet-quote")
+    .setAttribute(
+      "href",
+      `https://twitter.com/intent/tweet?text=${text} - ${author || "Anonymous"}`
+    );
+};
 
 getQuotes();
-document.getElementById('new-quote').addEventListener('click', newQuoteFromArray);
+document
+  .getElementById("new-quote")
+  .addEventListener("click", newQuoteFromArray);
